e60490cd7d5c9c5609d4186cb128b56843d68b7f
Bishesh Singh Ujjain <BU061787@cerner.com>
2020-05-05 12:02:54 +0530

    Implementation for making and retrieving service requests

---
+++ src/components/QuoteGeneratorCotainer.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+import QuoteGeneratorView from './QuoteGeneratorView';
+
+class QuoteGeneratorContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: '',
+            character: '',
+            quoteReceived: true,
+        }
+        this.handleGetQuote = this.handleGetQuote.bind(this);
+    }
+
+    /**
+     * handler function to retrieve quote service call request
+     * @param {*} event 
+     */
+    handleGetQuote(event) {
+        if (event) {
+            axios.get(
+                `https://thesimpsonsquoteapi.glitch.me/quotes`
+            ).then(response => {
+                const { quote, character } = response.data[0];
+                this.setState({
+                    quote: this.state.quoteReceived ? quote : 'Failed to load.Click on Try Again!!!',
+                    character: this.state.quoteReceived ? character : '',
+                    quoteReceived: true,
+                })
+            }).catch(err => {
+                this.setState({
+                    quoteReceived: false,
+                })
+            })
+        }
+    }
+    render() {
+        const { quote, character, quoteReceived } = this.state;
+        let buttonText = quoteReceived ? 'New Quote' : 'Try Again!!!';
+        //let quoteText = ;
+        //let author = quoteReceived ? { character } : '';
+
+        return (
+            <QuoteGeneratorView handleGetQuote={this.handleGetQuote} quote={quote} character={character} buttonText={buttonText} />
+        )
+    }
+}
+
+export default QuoteGeneratorContainer;
